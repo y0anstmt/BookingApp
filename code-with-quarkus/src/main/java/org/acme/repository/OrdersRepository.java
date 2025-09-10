9b452b6367e586bf3cda42983078e61756c60637
Ioan Stamatin <ioan.stamatin@stud.ubbcluj.ro>
2025-09-10 15:44:12 +0300

    Update OrdersRepository.java

--- code-with-quarkus/src/main/java/org/acme/repository/OrdersRepository.java
+++ code-with-quarkus/src/main/java/org/acme/repository/OrdersRepository.java
@@ -1,4 +1,4 @@
-package org.acme.repository;
+\=package org.acme.repository;
 
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
@@ -9,8 +9,7 @@ import java.util.Optional;
 @ApplicationScoped
 public class OrdersRepository implements PanacheRepository<Orders> {
     public Optional<Orders> findOrderByEmail(String email){
-        Orders order = new Orders();
-        order = find("email", order.getUser().getEmail()).firstResult();
+        Orders order = find("email", email).firstResult();
         if(order!=null){
             return Optional.of(order);
         }
