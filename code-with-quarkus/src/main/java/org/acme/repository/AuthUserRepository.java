76427ec125eb614e5aeac92a9a0ef31340744175
loghinserban <andrei.serban.loghin@stud.ubbcluj.ro>
2025-09-10 15:21:58 +0300

    created repositories

---
+++ code-with-quarkus/src/main/java/org/acme/repository/AuthUserRepository.java
@@ -0,0 +1,13 @@
+package org.acme.repository;
+
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+import org.acme.model.AuthUser;
+
+@ApplicationScoped
+public class AuthUserRepository implements PanacheRepository<AuthUser> {
+    public AuthUser findByEmail(String email) {
+        return find("email", email).firstResultOptional()
+                .orElseThrow(() -> new IllegalArgumentException("User with email " + email + " not found"));
+    }
+}
